ae9ef82d2cbfdee445f00d74b7e8f4127605dbae
aadirajuthup <aadi.rajuthup@gmail.com>
2021-04-29 05:13:43 +0530

    all outputs colored and optimizations

--- main.py
+++ main.py
@@ -18,22 +18,20 @@ def invalid():
 
 def identify(mac):
 	if len(mac) == 17 or len(mac) == 12:
-		
-		# Requests data from https://macvendors.co/api/<MAC ADDRESS HERE>
+		# Requests data from https://macvendors.co/api/<MAC ADDRESS HERE> and loades response in obj
 		request = urllib.Request(url+mac, headers={'User-Agent' : 'API Browser'})
 		response = urllib.urlopen(request)
-		
-		# Reads the json response to obj with codec utf-8
-		# reader = codecs.getreader('utf-8')
 		obj = json.load(response)
 		
-		# Prints out the results in obj
+		# Prints out obj
+		sys.stdout.write(CYAN)
 		print('\nCompany: ', obj['result']['company'])
 		print('Address: ', obj['result']['address'])
 		print('Type: ', obj['result']['type'],)
 		print('MAC Prefix: ', obj['result']['mac_prefix'])
 		print('Start HEX: ', obj['result']['start_hex'])
 		print('End HEX: ', obj['result']['end_hex'], '\n')
+		sys.stdout.write(RESET)
 		
 		if __name__ == '__main__':
 			main()
@@ -46,12 +44,12 @@ def identify(mac):
 def main():
 	try:
 		# Ask for user input and passes the input to identify function
+		sys.stdout.write(GREEN)
 		mac = input("[+] Enter MAC Address: ")
+		sys.stdout.write(RESET)
 		identify(mac)
 	except:
-		sys.stdout.write(YELLOW)
-		print('\n[x] Exiting...')
-		sys.stdout.write(RESET)
+		sys.stdout.write('\n')
 		exit()
 
 if __name__ == '__main__':
